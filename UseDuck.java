cf0100764daa323c2e6ffd7f2b468b0591298162
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-01-28 16:40:01 +0800

    Update UseDuck.java

--- UseDuck.java
+++ UseDuck.java
@@ -1,11 +1,20 @@
 class Duck {
+  int size;
   public Duck() {
+    size = 27;
     System.out.println("Quack!");
+    System.out.println("Size is " + size);
+  }
+  public Duck(int duckSize) {
+    size = duckSize;
+    System.out.println("Quack!");
+    System.out.println("Size is " + size);
   }
 }
 
 public class UseDuck {
   public static void main(String[] args) {
-    Duck d = new Duck();
+    Duck d1 = new Duck(42);
+    Duck d2 = new Duck();
   }
 }
