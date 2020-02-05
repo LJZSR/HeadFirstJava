16ade6346b6edbaaaed17d6013bba6f6961f6298
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-02-05 23:45:13 +0800

    Create Panel3.java

---
+++ Panel3.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel3 {
+  public static void main(String[] args) {
+    Panel3 gui = new Panel3();
+    gui.go();
+  }
+
+  public void go() {
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    panel.setBackground(Color.darkGray);
+
+    JButton button1 = new JButton("Shock Me");
+    JButton button2 = new JButton("Bliss");
+
+    panel.add(button1);
+    panel.add(button2);
+
+    frame.getContentPane().add(BorderLayout.EAST, panel);
+
+    frame.setSize(250, 200);
+    frame.setVisible(true);
+  }
+}
