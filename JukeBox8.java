7f522baf897836a1f1a3c187367a8844a4e705e9
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-02-10 21:05:10 +0800

    Create JukeBox8.java

---
+++ JukeBox8.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+class Song implements Comparable<Song> {
+  String title;
+  String artist;
+  String rating;
+  String bpm;
+
+  public Song(String t, String a, String r, String b) {
+    title = t;
+    artist = a;
+    rating = r;
+    bpm = b;
+  }
+
+  public boolean equals(Object aSong) {
+    Song s = (Song) aSong;
+    return getTitle().equals(s.getTitle());
+  }
+
+  public int hashCode() {
+    return title.hashCode();
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getArtist() {
+    return artist;
+  }
+
+  public String getRating() {
+    return rating;
+  }
+
+  public String getBpm() {
+    return bpm;
+  }
+
+  public String toString() {
+    return title;
+  }
+
+  public int compareTo(Song o) {
+    return title.compareTo(o.getTitle());
+  }
+
+}
+
+public class JukeBox8 {
+  ArrayList<Song> songList = new ArrayList<Song>();
+
+  public static void main(String[] args) {
+    new JukeBox8().go();
+  }
+
+  public void go() {
+
+    getSongs();
+
+    System.out.println(songList);
+    Collections.sort(songList);
+    System.out.println(songList);
+
+    TreeSet<Song> songSet = new TreeSet<Song>();
+    songSet.addAll(songList);
+    System.out.println(songSet);
+  }
+
+  public void getSongs() {
+    try {
+      File file = new File("SongListMore.txt");
+      BufferedReader reader = new BufferedReader(new FileReader(file));
+      String line = null;
+      while ((line = reader.readLine()) != null) {
+        addSong(line);
+      }
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void addSong(String line) {
+    String[] tokens = line.split("/");
+    songList.add(new Song(tokens[0], tokens[1], tokens[2], tokens[3]));
+  }
+}
